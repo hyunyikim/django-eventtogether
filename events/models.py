83b5c16adee6dcad2d58c184dc8c5ff80252913a
hyunyikim <developerhaileykim@gmail.com>
2019-04-27 10:14:35 +0900

    maria db 연동

--- events/models.py
+++ events/models.py
@@ -1,3 +1,28 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+class Events(models.Model):
+    eid = models.CharField(primary_key=True, max_length=15)
+    ename = models.CharField(max_length=100, blank=True, null=True)
+    econtent = models.TextField(blank=True, null=True)
+    ecreater = models.CharField(max_length=40, blank=True, null=True)
+    eloc = models.CharField(max_length=40, blank=True, null=True)
+    estartdt = models.DateField(blank=True, null=True)
+    estarttm = models.TimeField(blank=True, null=True)
+    eenddt = models.DateField(blank=True, null=True)
+    eendtm = models.TimeField(blank=True, null=True)
+    eprice = models.IntegerField(blank=True, null=True)
+    emaxattendee = models.IntegerField(blank=True, null=True)
+    eculattendee = models.IntegerField(blank=True, null=True)
+    eimage = models.CharField(max_length=40, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'events'
--- eventtogether/settings.py
+++ eventtogether/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hm!l)ak6_thwk$-%18x!lc6a=inw8#!#sp%6+5l0eoh**h&ezr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'events.apps.EventsConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'eventtogether.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'db_connect.cnf'),
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+        }
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
