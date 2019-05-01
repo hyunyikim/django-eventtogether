b118358d5adbf66eedce7485e66631819c0efea5
hyunyikim <developerhaileykim@gmail.com>
2019-05-01 12:49:58 +0900

    app verbose_name 설정

--- eventtogether/views.py
+++ eventtogether/views.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from django.apps import apps
 
 
 class HomeView(generic.TemplateView):
@@ -7,5 +8,10 @@ class HomeView(generic.TemplateView):
     # 만약 보낼 데이터가 있다면?
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['app_list'] = {'app': 'event together!'}
+        dictVerbose = {}
+        for app in apps.get_app_configs():
+            print('app : ', app.path)
+            if 'site-packages' not in app.path:
+                dictVerbose[app.label] = app.verbose_name
+        context['verbose_dict'] = dictVerbose
         return context
